9475a605580b31e85594a81ed94708cc4dbda3d4
Sarah Vayansky <sarahvayansky@gmail.com>
2019-04-03 17:46:24 -0400

    HTML stuff and multiple pages

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,5 @@ from homepage import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -8,3 +8,5 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+class AboutPageView(TemplateView):
+    template_name = "about.html"
