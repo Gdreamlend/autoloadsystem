decaad14e33233581a6b10c4e304ced434f3bb78
Jiaojiao Gao <gdreamlend@gmail.com>
2019-04-20 18:50:15 -0400

    slide

--- autoload_system/urls.py
+++ autoload_system/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "Slide Presentation Admin"
 admin.site.site_title = "Slide Presentation Admin Portal"
@@ -28,5 +29,4 @@ urlpatterns = [
     url(r'^', include('homepage.urls')),
     #path('admin/', admin.site.urls),
     #url(r'^main/', include('main.urls'), namespace='Main'),
-    url(r'^slide/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
