c77e8308017a639b8b78d41e22ce5040e6e787e6
Jiaojiao Gao <gdreamlend@gmail.com>
2019-04-20 17:54:16 -0400

    slide

--- homepage/admin.py
+++ homepage/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Slide
 from .models import Department
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 def make_approved(modeladmin, request, queryset):
     queryset.update(status='approved')
@@ -10,6 +11,8 @@ make_approved.short_description = "Approve all selected"
 def make_rejected(modeladmin, request, queryset):
     queryset.update(status='rejected')
 make_rejected.short_description = "Reject all selected"
+
+
 
 class SlideAdmin(admin.ModelAdmin):
     list_display = ('department','thumbnail','monitor','start_date','end_date','priority', 'description','status')
@@ -24,14 +27,21 @@ class SlideAdmin(admin.ModelAdmin):
     #                         height: 100px"/>')
     # thumbnail.short_description = 'thumbnail'
 
+    # def thumbnail(self, obj):
+    #     if obj.file:
+    #         return u'<img src="%s" />' % obj.file
+    #     else:
+    #         return '(No image found)'
+    #
+    # thumbnail.short_description = 'thumbnail'
+    # thumbnail.allow_tags = True
     def thumbnail(self, obj):
         if obj.file:
-            return u'<img src="%s" />' % obj.file
+            return mark_safe('<img src="%s"  height="100px"/>' % obj.file)
         else:
-            return '(No image found)'
-
-    thumbnail.short_description = 'thumbnail'
-    thumbnail.allow_tags = True
+            return 'No_image'
+        thumbnail.short_description = 'thumbnail'
+        thumbnail.allow_tags = True
 
 admin.site.register(Slide, SlideAdmin)
 admin.site.register(Department)
