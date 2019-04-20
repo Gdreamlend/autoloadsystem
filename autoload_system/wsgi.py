1d7b65114d4e4db871035947cdd9c68257fd57bd
gdreamlend <gdreamlend@gmail.com>
2019-04-20 20:43:15 +0000

    setting

--- autoload_system/wsgi.py
+++ autoload_system/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoload_system.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoload_system.settings')
+sys.path.append('/opt/bitnami/apps/django/django_projects/autoload_system')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/autoload_system/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autoload_system.settings")
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
