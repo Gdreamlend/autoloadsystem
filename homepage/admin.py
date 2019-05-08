b1ab05448581239ff941935633c7aab310efbf39
Jiaojiao Gao <gdreamlend@gmail.com>
2019-05-08 10:56:02 -0700

    gview

--- homepage/admin.py
+++ homepage/admin.py
@@ -42,9 +42,9 @@ class SlideAdmin(admin.ModelAdmin):
             # return format_html('<img src= "https://view.officeapps.live.com/op/embed.aspx?src=[{}]" style="width: 130px; \
             #                             height: 100px" />'.format(url))
 
-            return format_html('<img id = "iframe1" src = "http://docs.google.com/gview?url={}&embedded=true" \
-                onload = "javascript:this.contentWindow.location.hash=":0.page.20";" style="width: 100px; \
-            #                             height: 130px" />'.format(url))
+            return format_html('<iframe id = "iframe1" src = "http://docs.google.com/gview?url={}&embedded=true" \
+                onload = "javascript:this.contentWindow.location.hash=":0.page.20"; style="width: 130px; \
+            #                             height: 100px"></iframe>'.format(url))
         else:
             return '(No image found)'
 
